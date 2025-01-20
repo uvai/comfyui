0432428ed2b8061602cddbd35834d1b07ff8c8df
UVAI <38521971+uvai@users.noreply.github.com>
2025-01-20 18:39:51 +1100

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -22,11 +22,11 @@ PIP_PACKAGES=(
 NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
     "https://github.com/cubiq/ComfyUI_essentials"
-    "https://github.com/benjiyaya/ComfyUI-HunyuanVideoImagesGuider.git"
+    # "https://github.com/benjiyaya/ComfyUI-HunyuanVideoImagesGuider"
 )
 
 CHECKPOINT_MODELS=(
-    # "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
+    "https://civitai-delivery-worker-prod.5ac0637cfd0766c97916cefa3764fbdf.r2.cloudflarestorage.com/model/73783/pornmasterAmateurSDXL.CKu4.safetensors?X-Amz-Expires=86400&response-content-disposition=attachment%3B%20filename%3D%22pornmasterAmateur_sdxlV1VAE.safetensors%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=e01358d793ad6966166af8b3064953ad/20250118/us-east-1/s3/aws4_request&X-Amz-Date=20250118T114301Z&X-Amz-SignedHeaders=host&X-Amz-Signature=59f2b3f20582c1edacbc51d5bef890042cb8b9dba9cc2ffaf684b83707bdd615"
     #" https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     # "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     # "https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
@@ -41,8 +41,8 @@ LORA_MODELS=(
 )
 
 VAE_MODELS=(
-    "https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors"
-    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
+    # "https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors"
+    # "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
     "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
 )
 
